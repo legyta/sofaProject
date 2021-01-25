b515b4f08d8b3af425de8517c28f4e740c4ef7c6
Ligita <legyta@gmail.com>
2021-01-25 20:39:21 +0100

    soundtracks page done

--- src/components/pages/Soundtracks.js
+++ src/components/pages/Soundtracks.js
@@ -4,22 +4,33 @@ import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import soundtracks from "../stylesheets/soundtracks.css";
 import mariaBlanket from "../images/mariaBlanket.png";
+import soundtrack from "../images/soundtrack.svg";
+import play from "../images/play.svg";
+import Tracks from "../pages/Tracks.js";
 
 function Soundtracks() {
   return (
-    <Grid style={{ backgroundColor: "#e5989b" }}>
+    <Grid container style={{ backgroundColor: "#D95959" }}>
       <Link to="/soundtracks"></Link>
-      <Box className="soundtracks">
-        <Box>
-          <img src={mariaBlanket} alt="soundtracks" />
+      <Box className="photoAndDescription">
+        <Box
+          className="photo"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <img src={soundtrack} alt="soundtracks" id="soundtrackPhoto" />
         </Box>
-        <Box className="tracksList">
-          <h1>Soundtrack For Sofas</h1>
-          <p>1 I wanna COudle you</p>
-          <p>2 Doors are you ok?</p>
-          <p>3 Hip hop</p>
+        <Box className="description">
+          <p>
+            This is a soundtrack made for sofas created by object-music
+            composers – Mark Liberman, Mikas Zabulionis, doors of PAF, sofas of
+            Vyturys Hotel, human opinions and Marija Baranauskaitė.
+            <br></br>
+            <br></br>Please, put this soundtrack on for your sofas and become a
+            sofa yourself to receive the music in its intended way.{" "}
+          </p>
         </Box>
       </Box>
+      <Tracks />
     </Grid>
   );
 }
---
+++ src/components/pages/Tracks.js
@@ -0,0 +1,232 @@
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import play from "../images/play.svg";
+import pause from "../images/pause.svg";
+import tracks from "../stylesheets/tracks.css";
+import allAround from "../tracks/allAround.mp3";
+
+function Tracks() {
+  let audio = new Audio(allAround);
+
+  return (
+    <Grid style={{ margin: "40px 0px" }}>
+      <Box className="tracks1">
+        <Box
+          className="track1"
+          style={{
+            backgroundColor: "#A4CFC5",
+            padding: "20px",
+            margin: "20px",
+          }}
+        >
+          <Box
+            className="trackTitle"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <img
+              src={play}
+              alt="play"
+              id="play"
+              style={{ marginRight: "10px" }}
+              onClick={() => audio.play()}
+            />
+
+            <img
+              src={pause}
+              alt="pause"
+              id="play"
+              style={{ marginRight: "10px" }}
+              onClick={() => audio.pause()}
+            />
+
+            <h2 style={{ marginRight: "10px" }}>Cuddle You</h2>
+            <h2 style={{ fontWeight: "100" }}>5:00 min</h2>
+          </Box>
+
+          <Box className="trackDescription">
+            <p>
+              This song was made in France by Marija. In this song she expresses
+              her feelings towards and dreams about sofas of the world.{" "}
+            </p>
+          </Box>
+        </Box>
+        <Box
+          className="track1"
+          style={{
+            backgroundColor: "#A4CFC5",
+            padding: "20px",
+            margin: "20px",
+          }}
+        >
+          <Box
+            className="trackTitle"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <img
+              src={play}
+              alt="play"
+              id="play"
+              style={{ marginRight: "10px" }}
+            />
+            <h2 style={{ marginRight: "10px" }}>Doors, are you ok?</h2>
+            <h2 style={{ fontWeight: "100" }}>2:00 min</h2>
+          </Box>
+          <Box className="trackDescription">
+            <p>
+              This song is a collaboration between Marija, Mark and the doors of
+              PAF (Performing Arts Forum). Marija always felt like sofa-type
+              personality, whilst Mark found out he is a door type personality.
+              Thus, they have decided to invite the doors of PAF to help with
+              the Soundtrack for Sofas. It is never clear what objects are
+              feeling or saying, or whether they have any consciousness at all.
+              Because of this not-knowing, the music of doors is melancholic and
+              sad.
+            </p>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box className="tracks1">
+        <Box
+          className="track1"
+          style={{
+            backgroundColor: "#A4CFC5",
+            padding: "20px",
+            margin: "20px",
+          }}
+        >
+          <Box
+            className="trackTitle"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <img
+              src={play}
+              alt="play"
+              id="play"
+              style={{ marginRight: "10px" }}
+            />
+            <h2 style={{ marginRight: "10px" }}>
+              Party of doors with a bit of whales and babies
+            </h2>
+            <h2 style={{ fontWeight: "100" }}>3:00 min</h2>
+          </Box>
+          <Box className="trackDescription">
+            <p>
+              Whilst exploring door sounds, Marija’s friend shared the
+              soundtrack of her baby. This made Marija and Mark recognize the
+              intrinsic similarity between the sounds of doors and babies (and
+              later on, whales) – we don’t know what they say, but we can party.
+            </p>
+          </Box>
+        </Box>
+        <Box
+          className="track1"
+          style={{
+            backgroundColor: "#A4CFC5",
+            padding: "20px",
+            margin: "20px",
+          }}
+        >
+          <Box
+            className="trackTitle"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <img
+              src={play}
+              alt="play"
+              id="play"
+              style={{ marginRight: "10px" }}
+            />
+            <h2 style={{ marginRight: "10px" }}>
+              Human feedbacks and opinions on the sofa project
+            </h2>
+            <h2 style={{ fontWeight: "100" }}>2:59 min</h2>
+          </Box>
+          <Box className="trackDescription">
+            <p>
+              People always have a lot of questions and feedback about the sofa
+              project. During her solo show, Marija began collecting these human
+              opinions, which she decided to share with the sofas of the world.
+            </p>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box className="tracks1" style={{ marginTop: "0px" }}>
+        <Box
+          className="track1"
+          style={{
+            backgroundColor: "#A4CFC5",
+            padding: "20px",
+            margin: "20px",
+          }}
+        >
+          <Box
+            className="trackTitle"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <img
+              src={play}
+              alt="play"
+              id="play"
+              style={{ marginRight: "10px" }}
+            />
+            <h2 style={{ marginRight: "10px" }}>
+              Sofas and Human Touch Party{" "}
+            </h2>
+            <h2 style={{ fontWeight: "100" }}>6:06 min</h2>
+          </Box>
+          <Box className="trackDescription">
+            <p>
+              Humans are always interested in words and ideas. Sofas don’t care
+              about words and ideas (most probably). Marija discovered that it
+              is easier to connect with the public of sofas through touch and
+              physicality. While doing contemporary dance contact improvisation
+              research with sofas she has found that the sounds made by the
+              touch of humans and sofas reveals a lot about their relationship.
+              This was the moment when the idea of the soundtrack was conceived.
+              With the help of object-composer Mikas, she turned these sounds
+              into another party song.
+            </p>
+          </Box>
+        </Box>
+        <Box
+          className="track1"
+          style={{
+            backgroundColor: "#A4CFC5",
+            padding: "20px",
+            margin: "20px",
+          }}
+        >
+          <Box
+            className="trackTitle"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <img
+              src={play}
+              alt="play"
+              id="play"
+              style={{ marginRight: "10px" }}
+            />
+            <h2 style={{ marginRight: "10px" }}>
+              Guided Meditation towards stillness for sofas and half sofas half
+              humans
+            </h2>
+            <h2 style={{ fontWeight: "100" }}>5:00 min</h2>
+          </Box>
+          <Box className="trackDescription">
+            <p>
+              Relax. Make yourself as comfy as you can. Dive in. This is a
+              meditation made for both sofas and humans or half sofas half
+              humans. The intention of this meditation is to guide the audience
+              to total stillness and experience the pleasure of this state.
+            </p>
+          </Box>
+        </Box>
+      </Box>
+    </Grid>
+  );
+}
+
+export default Tracks;
